cfebed0898212184ce0e2f3b6354ab28175f9871
luizhen1 <oluizhen1@gmail.com>
2023-07-17 15:36:05 -0300

    atualizado

--- src/pages/trailer/TrailerPage.js
+++ src/pages/trailer/TrailerPage.js
@@ -5,12 +5,62 @@ import './TrailerPage.css'
 function Trailer(){
   return( 
 
-<div className="video">
+    <div class="about">
+      
+        <div class="inner-section">
+            <h1>Sobre nós</h1>
+            <p class="text">
+            A CodeMaster é uma empresa especializada no desenvolvimento e 
+            fornecimento de soluções de CRM. Nossa equipe é composta por profissionais
+            altamente qualificados e experientes, que possuem profundo conhecimento
+            em CRM e suas aplicações práticas em diversos setores e segmentos de
+            mercado. Nossas soluções de CRM abrangem uma ampla gama de recursos e
+            funcionalidades, incluindo gestão de contatos, automação de vendas, 
+            marketing digital, atendimento ao cliente e análise de dados. Com nossa
+            plataforma, as empresas podem centralizar e organizar informações sobre 
+            seus clientes, rastrear interações, criar campanhas de marketing 
+            direcionadas, gerenciar pipelines de vendas, fornecer suporte ao 
+            cliente eficiente e obter insights valiosos para tomar decisões 
+            estratégicas. Nossa abordagem personalizada permite que as empresas 
+            adaptem as soluções de CRM de acordo com suas necessidades específicas, 
+            garantindo que obtenham o máximo benefício de nossos produtos. 
+            Além disso, fornecemos suporte técnico especializado, treinamento e 
+            consultoria para garantir uma implementação bem-sucedida e uma adoção 
+            efetiva de nossas soluções. Seja você uma pequena empresa em crescimento 
+            ou uma organização de grande porte, a CodeMaster está pronta para 
+            ajudá-lo a aprimorar seus relacionamentos com os clientes, melhorar a 
+            eficiência operacional e impulsionar o crescimento do seu negócio por 
+            meio de soluções de CRM confiáveis e de alto desempenho.
+            </p>
+            <div class="skills">
+                <button>Contact Us</button>
+            </div>
+        </div>
+    </div>
+
+/*<div className="pagina">
+<div className='sobre'>
+      <h2>Sobre nós</h2>
+      </div>
+      <div className="video-container">
+        <iframe
+          width="1280"
+          height="720"
+          src='../../../videos/trailer.mp4' autoPlay loop
+          title="Video sobre a empresa"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="conteudo">
+      <h4>Alguma descrição sobre a empresa...</h4>
+      </div>
+    </div>
+    */
     
-<video src='../../../videos/trailer.mp4' autoPlay loop />
-</div>
 
   )
 }
 
 export default Trailer;
+
